240e8965f03b9fbd5c8f58c056522fc386737839
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-07-06 21:38:54 +0300

    Added conditional rendering for pages.

--- Fitness-App/src/app/app.component.ts
+++ Fitness-App/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 interface SideNavToggle {
   screenWidth: number;
   collapsed: boolean;
 }
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,6 +16,24 @@ export class AppComponent {
 
   isSideNavCollapsed = false;
   screenWidth = 0;
+  shouldShowNav = true;
+  shouldShowHeader = true;
+  shouldShowFooter = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const url = event.urlAfterRedirects;
+        console.log('Current URL:', url);
+        this.shouldShowNav = !['/', '/login', '/register'].includes(url);
+        this.shouldShowHeader = !['/', '/login', '/register'].includes(url);
+        this.shouldShowFooter = !['/', '/login', '/register'].includes(url);
+        console.log('shouldShowNav:', this.shouldShowNav);
+        console.log('shouldShowHeader:', this.shouldShowHeader);
+        console.log('shouldShowFooter:', this.shouldShowFooter);
+      }
+    });
+  }
 
   onToggleSideNav(data: SideNavToggle): void {
     this.screenWidth = data.screenWidth;
