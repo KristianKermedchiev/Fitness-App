b0a433c5e515ec70f6e8df2dc205ccfbb0387db0
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-07-06 18:26:18 +0300

    added new routes

--- Fitness-App/src/app/app-routing.module.ts
+++ Fitness-App/src/app/app-routing.module.ts
@@ -3,11 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent},
+  // { path: 'statistics', component: StatisticsComponent},
+  // { path: 'coupens', component: CoupensComponent},
+  // { path: 'pages', component: PagesComponent},
+  // { path: 'media', component: MediaComponent},
+  // { path: 'settings', component: SettingsComponent},
 ];
 
 @NgModule({
