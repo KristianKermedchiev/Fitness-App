3c7b8bc0fc2f75f0d6d333bd95ce5726a85ba1c9
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-07-06 23:16:28 +0300

    added the logout button's functionality and redirect upon successful login

--- Fitness-App/src/app/top-nav/top-nav.component.ts
+++ Fitness-App/src/app/top-nav/top-nav.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-top-nav',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./top-nav.component.css']
 })
 export class TopNavComponent {
+  constructor(private authService: AuthService, private router: Router) { }
 
+  logout(): void {
+    this.authService.logout()
+      .then(() => {
+        console.log('Logged out!');
+        this.router.navigate(['/']); 
+      })
+      .catch((error) => {
+        console.log('Logout error:', error);
+      });
+  }
 }
