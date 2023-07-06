b64ad738c73545454832a7198ee4124729e5814b
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-07-06 11:05:51 +0300

    Log In Works!

--- Fitness-App/src/app/login/login.component.ts
+++ Fitness-App/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor(private afAuth: AngularFireAuth, private router: Router) { }
+
+  async login() {
+    try {
+      const userCredential = await this.afAuth.signInWithEmailAndPassword(this.email, this.password);
+      console.log('Logged in successfully!');
+      this.router.navigate(['/']); // Adjust the desired route
+    } catch (error) {
+      console.error('Error signing in:', error);
+    }
+  }
 }
