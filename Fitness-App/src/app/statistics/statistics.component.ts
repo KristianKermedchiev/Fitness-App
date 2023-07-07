28594441f08ec7f6077b74f57fadb0106cbbfb21
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-07-07 16:09:09 +0300

    Changed colors and updated statistics page.

--- Fitness-App/src/app/statistics/statistics.component.ts
+++ Fitness-App/src/app/statistics/statistics.component.ts
@@ -7,14 +7,52 @@ import { Component } from '@angular/core';
 })
 
 export class StatisticsComponent {
-  isAccordionExpanded = false;
-  formData: any = {};
+  nutrient1: string;
+  nutrient2: string;
+  nutrient3: string;
+  nutrient4: string;
+  nutrient5: string;
+  nutrient6: string;
+  weight: string;
+  height: string;
+  age: string;
+  exercise1: string;
+  exercise2: string;
+  exercise3: string;
 
-  toggleAccordion(): void {
-    this.isAccordionExpanded = !this.isAccordionExpanded;
+  constructor() {
+    this.nutrient1 = '';
+    this.nutrient2 = '';
+    this.nutrient3 = '';
+    this.nutrient4 = '';
+    this.nutrient5 = '';
+    this.nutrient6 = '';
+    this.weight = '';
+    this.height = '';
+    this.age = '';
+    this.exercise1 = '';
+    this.exercise2 = '';
+    this.exercise3 = '';
   }
 
-  submitForm(): void {
-    console.log('Form submitted:', this.formData);
+  updateNutrition() {
+    // Handle the submit event for updating nutrition
+    // Access the nutrient values using this.nutrient1, this.nutrient2, this.nutrient3
+    // Perform necessary actions (e.g., API call, data manipulation, etc.)
+    // Clear the form fields if needed
+  }
+
+  updateWeight() {
+    // Handle the submit event for updating weight
+    // Access the weight, height, age values using this.weight, this.height, this.age
+    // Perform necessary actions (e.g., API call, data manipulation, etc.)
+    // Clear the form fields if needed
+  }
+
+  updateRoutine() {
+    // Handle the submit event for updating routine
+    // Access the exercise values using this.exercise1, this.exercise2, this.exercise3
+    // Perform necessary actions (e.g., API call, data manipulation, etc.)
+    // Clear the form fields if needed
   }
 }
