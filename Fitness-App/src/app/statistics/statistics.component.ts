55dc59cefe5b9e461dc28d7cfa5396d35a154f77
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-07-07 15:20:37 +0300

    Update

--- Fitness-App/src/app/statistics/statistics.component.ts
+++ Fitness-App/src/app/statistics/statistics.component.ts
@@ -7,22 +7,14 @@ import { Component } from '@angular/core';
 })
 
 export class StatisticsComponent {
-  dailyCalories: number = 0;
-  dailySteps: number = 0;
-  dailyWater: number = 0;
-  currentWeight: number = 0;
-  lastWeek: number = 0;
-  goalWeight: number = 0;
-  sleep: number = 0;
-  workoutRoutine: number = 0;
-  proteins: number = 0;
-  carbs: number = 0;
-  fats: number = 0;
+  isAccordionExpanded = false;
+  formData: any = {};
 
-  submitForm() {
-    // You can perform any logic here to handle the form submission
-    console.log('Form submitted:', this.dailyCalories, this.dailySteps, this.dailyWater, 
-    this.currentWeight, this.lastWeek, this.goalWeight, this.sleep, this.workoutRoutine, this.proteins,
-    this.carbs, this.fats);
+  toggleAccordion(): void {
+    this.isAccordionExpanded = !this.isAccordionExpanded;
+  }
+
+  submitForm(): void {
+    console.log('Form submitted:', this.formData);
   }
 }
