0f86dbe974b7308292c26ab28e79fa34f860c2b5
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-07-09 23:03:52 +0300

    Added HTML and CSS to the details page

--- Fitness-App/src/app/recipes/details/details.component.ts
+++ Fitness-App/src/app/recipes/details/details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-details',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
+  recipeId: string | null = null; // Initialize the recipeId property
 
+  recipe$: Observable<any> | undefined;
+
+  constructor(private route: ActivatedRoute, private firestore: AngularFirestore) {}
+
+  ngOnInit() {
+    this.recipeId = this.route.snapshot.paramMap.get('id');
+    if (this.recipeId) {
+      this.recipe$ = this.firestore.collection('recipes').doc(this.recipeId).valueChanges();
+    }
+  }
 }
