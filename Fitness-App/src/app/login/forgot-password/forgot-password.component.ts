49d1ca3db4daa28aa4af59f3e821d53bd684632a
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-07-10 17:58:38 +0300

    Password reset works!

--- Fitness-App/src/app/login/forgot-password/forgot-password.component.ts
+++ Fitness-App/src/app/login/forgot-password/forgot-password.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent {
+  
+  email: string = '';
 
+  constructor(private router: Router, private afAuth: AngularFireAuth) {}
+
+  routeToLogin(){
+    this.router.navigate(['/login']);
+  }
+
+  resetPassword() {
+    this.afAuth.sendPasswordResetEmail(this.email)
+      .then(() => {
+        console.log('password reset');
+        // You can show a success message to the user
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        // You can show an error message to the user
+      });
+  }
 }
